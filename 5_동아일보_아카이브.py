204c43679ca7109694fe664b689be085f95ed807
kimhsr <kimhsr@naver.com>
2022-01-21 19:38:05 +0900

    기사 전부 가져오기

--- 5_동아일보_아카이브.py
+++ 5_동아일보_아카이브.py
@@ -8,7 +8,15 @@ post_data = {
     "bid": "본인_아이디_입력",
     "bpw": "본인_비밀번호_입력"}
 sess.post("https://secure.donga.com/membership/trans_exe.php", headers=h, data=post_data)
-code = sess.get("https://www.donga.com/archive/newslibrary/view?idx=19920121%2F0002450920%2F1")
+
+# 뉴스 기사 제목 요소들 가져오기
+code = sess.get("https://www.donga.com/archive/newslibrary/view?ymd=19920121&mode=19920121/0002450920/1")
 soup = BeautifulSoup(code.text, "html.parser")
-content = soup.select_one("div.article_txt")
-print(content.text)
+a = soup.select("ul.news_list a")
+for i in a:
+    article_num = i.attrs["onclick"].replace("javascript:getNewsArticle('19920121/", "").replace("/1'); return false;", "")
+    url = "https://www.donga.com/archive/newslibrary/view?idx=19920121%2F{}%2F1".format(article_num)
+    code = sess.get(url)
+    soup = BeautifulSoup(code.text, "html.parser")
+    content = soup.select_one("div.article_txt")
+    print(content.text)
